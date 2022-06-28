0dcdefbe33bcf1872f4f5357f687cf57d56bccdd
shaiknafisa1607 <nafisashaik1607@gmail.com>
2022-06-28 16:10:13 +0530

    first commit

---
+++ lib/constants/routes.dart
@@ -0,0 +1,9 @@
+class Routes{
+  static const String PRODUCT_DETAIL_SCREEN ="/product-detail";
+  static const String CART_SCREEN ="/cart";
+  static const String ORDERS_SCREEN="/orders";
+  static const String PRODUCT_OVERVIEW_SCREEN="/products";
+  static const String USER_PRODUCTS_SCREEN ="/user-products";
+  static const String EDIT_PRODUCT_SCREEN ="/edit-product";
+  static const String AUTH_SCREEN="/auth";
+}
